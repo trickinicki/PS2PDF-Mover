bdb13ab704ae74d315865c6e44e1d5633398a8bf
unknown <C:\Users\Niklaus\AppData\Roaming\The Bat!>
2018-11-19 18:25:21 +0100

    extenting folderwatcher

--- PS2PDF-Mover/FolderWatcher.vb
+++ PS2PDF-Mover/FolderWatcher.vb
@@ -1,30 +1,46 @@
 ﻿Imports System.IO
 
+
 Module FolderWatcher
 
-	Private WithEvents m_fsw As IO.FileSystemWatcher
+	'	Private WithEvents m_fsw As New List(Of IO.FileSystemWatcher)
+	Private outFolder As String
 
-	Public Sub start(folderName As String)
-		MsgBox(folderName)
+	Public Sub start(aInfolder As String, aOutFolder As String)
 
-		m_fsw = New IO.FileSystemWatcher(folderName)
-		m_fsw.EnableRaisingEvents = True
+		outFolder = aOutFolder
 
-	End Sub
-
-	Private Sub m_fsw_Created(sender As Object, e As FileSystemEventArgs) Handles m_fsw.Created
-		Try
-			Await       MsgBox("hallo Welt")
-		Catch ex As Exception
-
-		End Try
+		Dim aNewfsw As New FileSystemWatcher(aInfolder)
+		AddHandler aNewfsw.Changed, AddressOf OnChanged
+		aNewfsw.EnableRaisingEvents = True
 
 
 	End Sub
 
-	Public Function IsFileReady(ByVal filename As String) As Boolean
-		Try
+	Public Sub stopWatching()
+		'	m_fsw.EnableRaisingEvents = False
+	End Sub
 
+	Private Async Sub OnChanged(sender As Object, e As FileSystemEventArgs)
+
+		If Await IsFileReady(e.FullPath) Then
+
+			'Datei umbenamsen
+			Dim newFileName As String = getJobNameFromPS(e.FullPath)
+
+			If File.Exists(outFolder & "\" & newFileName) Then File.Delete(outFolder & "\" & newFileName)
+
+			'Datei verschieben
+			File.Move(e.FullPath, outFolder & "\" & newFileName)
+
+		End If
+
+	End Sub
+
+
+	Public Async Function IsFileReady(ByVal filename As String) As Task(Of Boolean)
+		Try
+			Await Task.Delay(100)
 			Using inputStream As FileStream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None)
 				Return inputStream.Length > 0
 			End Using
@@ -33,4 +49,59 @@ Module FolderWatcher
 			Return False
 		End Try
 	End Function
+
+	Public Function getJobNameFromPS(filename As String) As String
+		'<job-name syntax= "name" xml:Space = "preserve" > Microsoft Word - Brief an Neff 26-06-2018.docx</job-name>
+		'<file-name syntax="text" xml:space="preserve">Microsoft Word - Brief an Neff 26-06-2018.docx</file-name>
+		Dim aLine As String
+		Dim reader As New StreamReader(filename, System.Text.Encoding.Default)
+
+		Dim lineNames As New List(Of String)
+		lineNames.Add("job-name")
+		lineNames.Add("file-name")
+
+
+		Dim fileExtension As New List(Of String)
+		fileExtension.Add(".docx")
+		fileExtension.Add(".doc")
+		fileExtension.Add(".xlsx")
+		fileExtension.Add(".xls")
+		fileExtension.Add(".indd")
+
+		Dim relpaceTexts As New List(Of String)
+		relpaceTexts.Add("Microsoft Word - ")
+		relpaceTexts.Add("Microsoft Excel - ")
+
+		Do
+			aLine = reader.ReadLine
+			For Each aName As String In lineNames
+				If aLine.IndexOf(aName) > 0 Then
+					reader.Close()
+
+					Dim startIndex As Integer = aLine.IndexOf(">")
+					Dim StopIndex As Integer = aLine.IndexOf("<", startIndex)
+
+					Dim newName As String = aLine.Substring(startIndex + 1, StopIndex - startIndex - 1)
+					For Each aExtension In fileExtension
+						newName = newName.Replace(aExtension, "")
+					Next
+
+					For Each aText In relpaceTexts
+						newName = newName.Replace(aText, "")
+					Next
+
+
+					Return newName & ".ps"
+
+				End If
+			Next
+
+		Loop Until aLine Is Nothing
+		reader.Close()
+
+		Return "Unbekanntes Dokument" & CStr(Date.Today).Replace(".", "-") & ".ps"
+
+
+	End Function
+
 End Module
